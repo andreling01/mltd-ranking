b7df5a3bf7a8da01c5617b6024af9a76f481e59e
Zheng Ling <andreling01@hotmail.com>
2022-07-04 15:00:22 -0700

    Finished basic function and sorting function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import React, { Component } from "react"
 function App() {
   return (
     <div className="table_container">
-        <h1>Sortable table with React</h1>
+        <h1>灭了同担五周年排行榜</h1>
+        <h3>由于matsurihi.me有访问限制，初次读取大约需要10-20秒</h3>
+        <h3>点击表格列的标题可按该列排序</h3>
         <IdolTable />
     </div>
   );
--- src/components/Table/IdolTable.js
+++ src/components/Table/IdolTable.js
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import idolMapping from "../../idolMapping.json"
 import axios from 'axios';
-import styled from "styled-components"
-import IdolTableRow from "./IdolTableRow"
 
 export default function IdolTable(props) {
-
         const sleep = (milliseconds) => {
             return new Promise(resolve => setTimeout(resolve, milliseconds))
         }
@@ -26,83 +23,130 @@ export default function IdolTable(props) {
 
         const [idols, setIdols] = useState(initialIdolState);
 
-    useEffect(() => {
-        let baseUrl = "https://api.matsurihi.me/mltd/v1/events/241/rankings/logs/idolPoint/"
-        let query = "/1,100,1000";
-        let results = [];
-        async function fetchData(id) {
-            let request = baseUrl + id + query;
-            axios.get(request).then((result) => {
-                results.push(result);
-            }).catch((error) => {console.log(error);});
-            await sleep(200);
-        };
+        const useSortableData = (items, config = null) => {
+            const [sortConfig, setSortConfig] = useState(config);
 
-        async function fetchAll() {
-          for (let i = 1; i <= 52; i++) {
-            await fetchData(i);
-          }
-        }
-
-
-        async function build() {
-            await fetchAll();
-            for (let i = 0; i < 52; i++) {
-                let first,hundred, thousand = 0;
-                if (results[i].data[0]) {
-                    first = results[i].data[0].data[results[i].data[0].data.length - 1].score;
+            const sortedItems = useMemo(() => {
+                let sortableItems = [...items];
+                if (sortConfig !== null) {
+                    sortableItems.sort((a, b) => {
+                        if (a[sortConfig.key] < b[sortConfig.key]) {
+                            return sortConfig.direction === 'ascending' ? -1 : 1;
+                        }
+                        if (a[sortConfig.key] > b[sortConfig.key]) {
+                            return sortConfig.direction === 'ascending' ? 1 : -1;
+                        }
+                        return 0;
+                    });
                 }
+                return sortableItems;
+            }, [items, sortConfig]);
 
-                if (results[i].data[1]) {
-                    hundred = results[i].data[1].data[results[i].data[1].data.length - 1].score;
+            const requestSort = key => {
+                let direction = 'ascending';
+                if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+                direction = 'descending';
                 }
-
-                if (results[i].data[2]) {
-                    thousand = results[i].data[2].data[results[i].data[2].data.length - 1].score;
-                }
-
-                setIdols(current =>
-                  current.map(obj => {
-                    if (obj.id === i + 1) {
-                      return {...obj, first : first, hundred: hundred, thousand: thousand };
-                    }
-
-                    return obj;
-                  }),
-                );
+                setSortConfig({ key, direction });
             }
 
-            console.log(idols);
+            return { items: sortedItems, requestSort };
         }
 
-        build();
-    },[]);
+        const { items, requestSort } = useSortableData(idols);
+
+
+        useEffect(() => {
+            let baseUrl = "https://api.matsurihi.me/mltd/v1/events/241/rankings/logs/idolPoint/"
+            let query = "/1,100,1000";
+            let results = [];
+            async function fetchData(id) {
+                let request = baseUrl + id + query;
+                axios.get(request).then((result) => {
+                    results.push(result);
+                }).catch((error) => {console.log(error);});
+                await sleep(200);
+            };
+
+            async function fetchAll() {
+              for (let i = 1; i <= 52; i++) {
+                await fetchData(i);
+              }
+            }
+
+
+            async function build() {
+                await fetchAll();
+                for (let i = 0; i < 52; i++) {
+                    let first,hundred, thousand = 0;
+                    if (results[i].data[0]) {
+                        first = results[i].data[0].data[results[i].data[0].data.length - 1].score;
+                    }
+
+                    if (results[i].data[1]) {
+                        hundred = results[i].data[1].data[results[i].data[1].data.length - 1].score;
+                    }
+
+                    if (results[i].data[2]) {
+                        thousand = results[i].data[2].data[results[i].data[2].data.length - 1].score;
+                    }
+
+                    setIdols(current =>
+                      current.map(obj => {
+                        if (obj.id === i + 1) {
+                          return {...obj, first : first, hundred: hundred, thousand: thousand };
+                        }
+
+                        return obj;
+                      }),
+                    );
+                }
+
+                console.log(idols);
+            }
+
+            build();
+        },[]);
 
 
 
        return (
            <table className="table">
-             <TableHeader />
+               <caption>数据读取自<a href="https://api.matsurihi.me/">api.matsurihi.me</a></caption>
+               <thead>
+                <tr>
+                 <th>
+                     <button type="button" onClick={() => requestSort('id')}>
+                         名字
+                     </button>
+                 </th>
+                 <th>
+                     <button type="button" onClick={() => requestSort('first')}>
+                        1位
+                     </button>
+                 </th>
+                 <th>
+                     <button type="button" onClick={() => requestSort('hundred')}>
+                        100位
+                     </button>
+                 </th>
+                 <th>
+                     <button type="button" onClick={() => requestSort('thousand')}>
+                        1000位
+                     </button>
+                 </th>
+                 </tr>
+               </thead>
                <tbody>
-               {(() => {
-                   const tableRows = [];
-                   for (let i = 0; i < 52; i++) {
-                     tableRows.push(<IdolTableRow idolName={idols[i].name} idolFirst={idols[i].first}
-                        idolHundred={idols[i].hundred} idolThousand={idols[i].thousand}/>);
-                   }
-                   return tableRows;
-               })()}
+                    {items.map(idol => (
+                        <tr key={idol.id}>
+                            <td>{idol.name}</td>
+                            <td>{idol.first}</td>
+                            <td>{idol.hundred}</td>
+                            <td>{idol.thousand}</td>
+                        </tr>
+                    ))}
                </tbody>
            </table>
        )
 }
-
-const TableHeader = () =>
-  <thead>
-   <tr>
-    <th>名字</th>
-    <th>1位</th>
-    <th>100位</th>
-    <th>1000位</th>
-    </tr>
-  </thead>
