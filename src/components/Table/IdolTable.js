629022be7c544c0a1106cbfd65c9c42c5b0b23a9
Zheng Ling <andreling01@hotmail.com>
2022-07-04 14:22:28 -0700

    List function works

--- src/components/Table/IdolTable.js
+++ src/components/Table/IdolTable.js
@@ -6,52 +6,94 @@ import IdolTableRow from "./IdolTableRow"
 
 export default function IdolTable(props) {
 
-
-    useEffect(() => {
-        const results = [];
-        for (let i = 1; i <= 52; i++) {
-            const fn = async () => {
-                let baseUrl = "https://api.matsurihi.me/mltd/v1/events/241/rankings/logs/idolPoint/" + i;
-                let request = baseUrl + "/1,100,1000";
-                let res = await axios.get(request, {});
-                results.push(res);
-            }
-            fn();
+        const sleep = (milliseconds) => {
+            return new Promise(resolve => setTimeout(resolve, milliseconds))
         }
 
-//        Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
-//            (data) => {
-//              console.log(data);
-//
-////              const responseOne = data[0];
-////              const responseHundred = data[1];
-////              const responseThousand = data[2];
-////              idols[i] = Object.assign({}, newIdol)
-////              idols[i].id = i;
-////              idols[i].first = responseOne.data[0].data[responseOne.data[0].data.length - 1].score;
-////              idols[i].hundred = responseHundred.data[0].data[responseHundred.data[0].data.length - 1].score;
-////              idols[i].thousand = responseThousand.data[0].data[responseThousand.data[0].data.length - 1].score;
-//            },
-//        );
+        const results = [];
+        const initialIdolState = [];
 
+        for (let i = 1; i <= 52; i++) {
+            const newIdol = {
+                id: i,
+                name: idolMapping[i],
+                first: 0,
+                hundred: 0,
+                thousand: 0
+            };
+            initialIdolState.push(newIdol);
+        }
+
+        const [idols, setIdols] = useState(initialIdolState);
+
+    useEffect(() => {
+        let baseUrl = "https://api.matsurihi.me/mltd/v1/events/241/rankings/logs/idolPoint/"
+        let query = "/1,100,1000";
+        let results = [];
+        async function fetchData(id) {
+            let request = baseUrl + id + query;
+            axios.get(request).then((result) => {
+                results.push(result);
+            }).catch((error) => {console.log(error);});
+            await sleep(200);
+        };
+
+        async function fetchAll() {
+          for (let i = 1; i <= 52; i++) {
+            await fetchData(i);
+          }
+        }
+
+
+        async function build() {
+            await fetchAll();
+            for (let i = 0; i < 52; i++) {
+                let first,hundred, thousand = 0;
+                if (results[i].data[0]) {
+                    first = results[i].data[0].data[results[i].data[0].data.length - 1].score;
+                }
+
+                if (results[i].data[1]) {
+                    hundred = results[i].data[1].data[results[i].data[1].data.length - 1].score;
+                }
+
+                if (results[i].data[2]) {
+                    thousand = results[i].data[2].data[results[i].data[2].data.length - 1].score;
+                }
+
+                setIdols(current =>
+                  current.map(obj => {
+                    if (obj.id === i + 1) {
+                      return {...obj, first : first, hundred: hundred, thousand: thousand };
+                    }
+
+                    return obj;
+                  }),
+                );
+            }
+
+            console.log(idols);
+        }
+
+        build();
     },[]);
 
 
+
        return (
-            "template"
-//           <table className="table">
-//             <TableHeader />
-//               <tbody>
-////               {(() => {
-////                   const tableRows = [];
-////                   for (let i = 1; i <= 52; i++) {
-//////                     tableRows.push(<IdolTableRow idol={idols[i]} />);
-////                        tableRows.push(idols[i]);
-////                   }
-////                   return tableRows;
-////               })()}
-//               </tbody>
-//           </table>
+           <table className="table">
+             <TableHeader />
+               <tbody>
+               {(() => {
+                   const tableRows = [];
+                   for (let i = 0; i < 52; i++) {
+                     tableRows.push(<IdolTableRow idolName={idols[i].name} idolFirst={idols[i].first}
+                        idolHundred={idols[i].hundred} idolThousand={idols[i].thousand}/>);
+                   }
+                   return tableRows;
+               })()}
+               </tbody>
+           </table>
        )
 }
 
--- src/components/Table/IdolTableRow.js
+++ src/components/Table/IdolTableRow.js
@@ -6,46 +6,20 @@ import PropTypes from "prop-types";
 
 
 export default function IdolTableRow(props) {
-    const [first, setFirst] = useState(0);
-    const [hundred, setHundred] = useState(0);
-    const [thousand, setThousand] = useState(0);
-
-    useEffect(() => {
-        let baseUrl = "https://api.matsurihi.me/mltd/v1/events/241/rankings/logs/idolPoint/" + props.id;
-        let requestOne = baseUrl + "/1";
-        let requestHundred = baseUrl + "/100";
-        let requestThousand = baseUrl + "/1000";
-
-        let endpoints = [requestOne, requestHundred, requestThousand];
-
-        axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
-          (data) => {
-            console.log(data);
-            const responseOne = data[0];
-            const responseHundred = data[1];
-            const responseThousand = data[2];
-
-            setFirst(responseOne.data[0].data[responseOne.data[0].data.length - 1].score);
-            setHundred(responseHundred.data[0].data[responseHundred.data[0].data.length - 1].score);
-            setThousand(responseThousand.data[0].data[responseThousand.data[0].data.length - 1].score);
-          },
-        );
-    },[]);
-
 
     return (
         <tr>
             <td>
-                {idolMapping[props.idol.id]}
+                {props.idolName}
             </td>
             <td>
-                {props.idol.first}
+                {props.idolFirst}
             </td>
             <td>
-                {props.idol.hundred}
+                {props.idolHundred}
             </td>
             <td>
-                {props.idol.thousand}
+                {props.idolThousand}
             </td>
         </tr>
     )
@@ -53,7 +27,11 @@ export default function IdolTableRow(props) {
 }
 
 IdolTableRow.propTypes = {
-    idol:PropTypes.shape.isRequired,
+    idolName: PropTypes.string,
+    idolFirst: PropTypes.string,
+    idolHundred: PropTypes.string,
+    idolThousand: PropTypes.string,
+
 };
 
 
